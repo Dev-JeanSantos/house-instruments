6bf7b1d3abadbad8dbf0edb87aeb18c37192e8b9
Dev-JeanSantos <jeancbsan@gmail.com>
2023-03-06 11:58:29 -0300

    starting the menubar component

---
+++ front-web/src/components/Menubar/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+import "@popperjs/core";
+
+const Menubar = () => {
+    return (
+        <div className="container-menubar">
+            
+        </div>
+    )
+}
+
+
+export default Menubar
