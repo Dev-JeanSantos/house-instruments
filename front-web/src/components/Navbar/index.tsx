7c60545f1c9ac866d07d0118f71c9999389e429a
Dev-JeanSantos <jeancbsan@gmail.com>
2023-03-06 10:39:48 -0300

    module error corretion

--- front-web/src/components/Navbar/index.tsx
+++ front-web/src/components/Navbar/index.tsx
@@ -1,9 +1,17 @@
 import "./styles.css";
 import "@popperjs/core";
+import { ReactComponent as Logo } from "../../assets/images/logo.svg";
 
 const Navbar = () => {
-    return(
-        <h1>Ola Mundo</h1>
+    return (
+        <nav className="navbar navbar-expand-md main-navbar">
+            <div className="container-fluid">
+                <div className="container-image">
+                    <Logo />
+                </div>
+            </div>
+
+        </nav>
     )
 }
 
