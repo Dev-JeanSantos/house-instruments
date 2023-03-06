975a7cbc9896d25f2e35e3a84cb13e469ba3a953
Dev-JeanSantos <jeancbsan@gmail.com>
2023-03-06 11:59:08 -0300

    starting the menubar component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import "./assets/styles/custom.scss"
+import Menubar from './components/Menubar';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Menubar />
+    </>
   );
 }
 
--- front-web/src/components/Navbar/index.tsx
+++ front-web/src/components/Navbar/index.tsx
@@ -1,13 +1,22 @@
 import "./styles.css";
 import "@popperjs/core";
+import Select from "react-select";
 import { ReactComponent as Logo } from "../../assets/images/logo.svg";
+
+const options = [
+    { value: 'violao', label: 'Violão'},
+    { value: 'violino', label: 'Violino'},
+    { value: 'violocelo', label: 'Violocelo'},
+    { value: 'viola', label: 'Viola'}
+]
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-md main-navbar">
             <div className="container-fluid">
-                <div className="container-image">
-                    <Logo />
+                <div className="container-navbar">
+                    <Logo className="container-image" />
+                    <Select className="container-select" options={options} placeholder="Buscar"/>
                 </div>
             </div>
 
